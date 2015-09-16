b24414c9348ebf0aacd7ae5827927cc5f8fe96b5
Hung Trinh <trinhduchung@gmail.com>
2015-09-17 00:10:51 +0700

    Skeleton of specification student writer controller

--- module/Achievement/test/AchievementTest/Controller/StudentControllerTest.php
+++ module/Achievement/test/AchievementTest/Controller/StudentControllerTest.php
@@ -4,8 +4,23 @@ namespace AchievementTest\Controller;
 
 class StudentControllerTest extends AbstractHttpControllerTestCase
 {
-    public function testTrue()
+    public function testWhenVisitPageCreateStudentProfileThenShowBlankStudentProfileForm()
     {
-        $this->assertTrue(true);
+        $this->assertTrue(false);
+    }
+
+    public function testWhenCreatedStudentProfileSuccessThenRedirectToPageListStudent()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testWhenVisitPageEditStudentProfileThenShowPopulatedStudentProfileForm()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testWhenUpdatedStudentProfileSuccessThenRedirectToPageListStudent()
+    {
+        $this->assertTrue(false);
     }
 }
