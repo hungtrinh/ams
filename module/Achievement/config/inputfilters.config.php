95bc737352a3c2e4769f5ad54e5b5890708da2cd
HungTd <trinhduchung@gmail.com>
2015-12-17 11:04:08 +0700

    Comment end of array with name of array key

--- module/Achievement/config/inputfilters.config.php
+++ module/Achievement/config/inputfilters.config.php
@@ -35,7 +35,7 @@ return [
                 'password' => [
                     'name' => 'password',
                 ]
-            ],
-        ],
-    ],
+            ], //account
+        ], //student
+    ], //Achievement\InputFilter\Student
 ];
