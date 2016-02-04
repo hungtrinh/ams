e2ca7143a271cc64bcc7dc4f66efdbc7c7332a55
HungTd <trinhduchung@gmail.com>
2016-02-04 16:39:07 +0700

    Refactoring code

--- FunctionalTest/test/Page/Student/RegisterNewStudentTest.php
+++ FunctionalTest/test/Page/Student/RegisterNewStudentTest.php
@@ -22,39 +22,39 @@ class RegisterNewStudentTest extends Selenium2TestCase
         $this->homepage = new Home($this);
     }
 
-    // public function testGivenOnHomePageWhenVisitPageRegisterStudentThenFormRegisterAppearCorrect()
-    // {
-    //     $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
-    //     $registerStudentPage->assertFormAppearCorrect();
-    // }
+    public function testGivenOnHomePageWhenVisitPageRegisterStudentThenFormRegisterAppearCorrect()
+    {
+        $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
+        $registerStudentPage->assertFormAppearCorrect();
+    }
 
-    // public function testGivenOnStudentRegisterPageWhenClickToDobThenCalendarDisplayed()
-    // {
-    //     $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
-    //     $calendarPanel = $registerStudentPage->clickToDobThenShowCalendar();
-    //     $calendarPanel->assertDisplayed();
-    // }
+    public function testGivenOnStudentRegisterPageWhenClickToDobThenCalendarDisplayed()
+    {
+        $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
+        $calendarPanel = $registerStudentPage->clickToDobThenShowCalendar();
+        $calendarPanel->assertDisplayed();
+    }
 
-    // public function testGivenOnCalendarForDobWhenClickToDayThenFillDateClicked2Dob()
-    // {
-    //     $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
-    //     $registerStudentPage->assertClickToDayOnCalendarWillFillDateToDob();
-    // }
+    public function testGivenOnCalendarForDobWhenClickToDayThenFillDateClicked2Dob()
+    {
+        $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
+        $registerStudentPage->assertClickToDayOnCalendarWillFillDateToDob();
+    }
 
-    // public function testGivenOnStudentRegisterPageWhenClickToSiblingsDobThenCalendarDisplayed()
-    // {
-    //     $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
-    //     $calendarPanel = $registerStudentPage->clickToSiblingDobThenShowCalendar();
-    //     $calendarPanel->assertDisplayed();
-    // }
+    public function testGivenOnStudentRegisterPageWhenClickToSiblingsDobThenCalendarDisplayed()
+    {
+        $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
+        $calendarPanel = $registerStudentPage->clickToSiblingDobThenShowCalendar();
+        $calendarPanel->assertDisplayed();
+    }
 
-    // public function testGivenOnCalendarForSiblingsDobWhenClickDayThenFillDateClicked2SiblingsDob()
-    // {
-    //     $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
-    //     $registerStudentPage->assertClickToDayOnCalendarWillFillDateToSiblingDob();
-    // }
+    public function testGivenOnCalendarForSiblingsDobWhenClickDayThenFillDateClicked2SiblingsDob()
+    {
+        $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
+        $registerStudentPage->assertClickToDayOnCalendarWillFillDateToSiblingDob();
+    }
 
-    public function testGivenOnStudentRegisterPageWhenTabOnSiblingWorkInputThenCreateNewSiblingFieldset()
+    public function testGivenOnStudentRegisterPageWhenTabOnSiblingWorkInputThenCreateNewSiblingFieldsetMax3()
     {
         $registerStudentPage = $this->homepage->gotoRegisterStudentPage();
         $registerStudentPage->assertOnSiblingWorkPressTabThenCreateMaximum3SiblingInput();
